3b645bab9781be3b45f6fce9d9af001d4f6b9341
Pablo Castillo <pablocastillo325@gmail.com>
2026-01-19 23:17:49 -0400

    certificates section

--- src/config.ts
+++ src/config.ts
@@ -11,14 +11,21 @@ export interface Project {
   title: string;
   description: string;
   tags: string[];
-  link: string;
-  github: string;
+  link?: string;
+  github?: string;
   image?: string;
+}
+
+export interface Certificate {
+  title: string;
+  issuer: string;
+  date: string;
+  link?: string;
 }
 
 export const config = {
   name: "Pablo Castillo",
-  role: "Full Stack Developer",
+  role: "Desarrollador Full Stack",
   email: "pablo.castillo@gmail.com",
   socials: {
     github: "https://github.com",
@@ -26,7 +33,7 @@ export const config = {
   },
   experience: [
     {
-      title: "Full-Stack Developer",
+      title: "Desarrollador Full Stack",
       company: "Waku Casa De Software",
       date: "Febrero 2022 - Presente",
       location: "Venezuela, Zulia, Maracaibo",
@@ -34,7 +41,7 @@ export const config = {
       description: "Optimicé la experiencia de usuario (UX) y flujos de trámites de un sistema de recaudación de impuestos usado por +100k usuarios activos en +3 estados de Venezuela. Logré integrar con éxito la conciliación de pagos con la banca nacional venezolana y de resolver errores críticos en momentos de alta demanda."
     },
     {
-      title: "Full-Stack Developer",
+      title: "Desarrollador Full Stack",
       company: "a2 Softway",
       location: "Venezuela, Zulia, Maracaibo",
       date: "Marzo 2021 - Febrero 2022",
@@ -42,7 +49,7 @@ export const config = {
       description: "Participé en el desarrollo y mantenimiento de un sistema de contabilidad web (ERP), colaborando en la implementación de funcionalidades clave tanto en el Back-End como en el Front-End."
     },
     {
-      title: "Web Developer",
+      title: "Desarrollador Web",
       company: "Insiemp",
       location: "Venezuela, Zulia, Maracaibo",
       date: "Marzo 2021 - Febrero 2022",
@@ -52,7 +59,7 @@ export const config = {
   ] as Experience[],
   projects: [
     {
-      title: "Sistema Unico de Trámites (SUT)",
+      title: "Sistema Único de Trámites (SUT)",
       description: "El SUT es una plataforma integral de gobierno electrónico que permite aumentar los ingresos a través de la recaudación de impuestos digitalizada y automatizar todos los trámites e interacciones con la ciudadanía desde cualquier computadora o dispositivo móvil. El sistema se utiliza en varios estados de Venezuela como San Cristóbal, Anzoátegui, Yaracuy y Barinas gestionando +100k usuarios activos y procesando miles de transacciones diarias.",
       tags: ["React", "TypeScript", "Ant Design", "Redux", "NodeJS", "ExpressJS", "PostgreSQL"],
       image: "/projects/sut.png"
@@ -82,6 +89,8 @@ export const config = {
     { name: "Bootstrap", icon: "devicon-bootstrap-plain"},
     { name: "jQuery", icon: "devicon-jquery-plain" },
     { name: "TailwindCSS", icon: "devicon-tailwindcss-plain"},
+    { name: 'Ant Design', icon: ''},
+    { name:'Redux', icon:''},
     { name: "Astro ", icon: "devicon-astro-plain"},
     { name: "React", icon: "devicon-react-original" },
     { name: "React Native", icon: "devicon-react-original" },
@@ -94,4 +103,24 @@ export const config = {
     { name: "PHP", icon: "devicon-php-plain" },
     { name: "Laravel", icon: "devicon-laravel-plain" },
   ],
+  certificates: [
+    {
+      title: "Certificado de Inglés",
+      issuer: "CEVAZ",
+      date: "2025",
+      link: "https://www.cevaz.org/"
+    },
+    {
+      title: "Complete Wordpress Website Developer Course",
+      issuer: "Udemy",
+      date: "2023",
+      link: "https://www.udemy.com/certificate/UC-69407787-4994-471d-a71a-fb0fb29cc4e3/"
+    },
+    {
+      title: "Front-End Web UI Frameworks and Tools: Bootstrap 4",
+      issuer: "Coursera",
+      date: "2021",
+      link: "https://www.coursera.org/account/accomplishments/verify/QXA9T37M44S2"
+    },
+  ] as Certificate[],
 };
